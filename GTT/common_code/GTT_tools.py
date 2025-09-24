4beea02ed41e248548aac75e82b24a28a32cdf42
Randy LeVeque <rjl@uw.edu>
2025-09-23 21:20:41 -0700

    fix setting GTT in GTT_tools and add check_cache function

--- GTT/common_code/GTT_tools.py
+++ GTT/common_code/GTT_tools.py
@@ -2,13 +2,11 @@ import os,sys
 import shutil
 import pooch
 
-#GTT = os.environ['GTT']
-#GTT = os.path.abspath('..')
+#GTT = os.environ['GTT']  # Doesn't work on Github
 
-s = os.path.abspath('.')
-while s[-3:] != 'GTT':
-    s,s2 = os.path.split(s)
-GTT = s
+#print('file = ', __file__)  # full path to this file
+common_code_dir = os.path.split(__file__)[0]  # drop filename
+GTT = os.path.split(common_code_dir)[0]  # drop subdirectory
 print('setting GTT = ',GTT)
 
 # remote repository:
@@ -24,11 +22,48 @@ GTT_data_repository = GTT + '_data_repository'
 #print('path to GTT_data_repository is\n     ', GTT_data_repository)
 registry_file_path = os.path.join(GTT_data_repository, 'registry.txt')
 
+# Create object to be used for fetching files:
 GTTdata = pooch.create(path=GTT_cache,
                        base_url=data_repository_url,
                        registry=None)
 
+# load registry file:
 GTTdata.load_registry(registry_file_path)
+
+def check_cache(data_paths=GTTdata.registry_files):
+    """
+    For each data_path in data_paths (by default all files in the registry),
+    check the hash of the data_path in cache against GTTdata.registry[data_path]
+    to see if it is out of date.
+    """
+    missing = []
+    out_of_date = []
+    for data_path in data_paths:
+        cached_file = os.path.join(GTT_cache, data_path)
+        if not os.path.isfile(cached_file):
+            missing.append(data_path)
+        else:
+            cached_file_hash = pooch.file_hash(cached_file)
+            registry_hash = GTTdata.registry[data_path]
+            if cached_file_hash != registry_hash:
+                out_of_date.append(data_path)
+
+    print('The following files in GTT_cache are up to date:')
+    for data_path in data_paths:
+        if (data_path not in missing) and (data_path not in out_of_date):
+            print('    ', data_path)
+
+    if len(missing) > 0:
+        print('The following files are missing from GTT_cache:')
+        for data_path in missing:
+            print('    ', data_path)
+
+    if len(out_of_date) > 0:
+        print('The following files in GTT_cache are out of date:')
+        for data_path in out_of_date:
+            print('    ', data_path)
+
+
 
 def fetch(file_path, destination=None, force=False, verbose=False):
 
